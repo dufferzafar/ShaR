7197f1c93630da6d440510d2375043e4bfabeee6
Shadab Zafar <shadab.softs@gmail.com>
2013-10-07 03:35:41 +0530

    Update...

--- v1.2.2.lua
+++ v1.2.2.lua
@@ -19,56 +19,56 @@ tbl_ALPHACAPS = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 --[[
 	Updated versions of IsUpper(), IsLower(), IsNumber()
 	Previously I used to traverse appropriate table till the argument matched with any of the table values.
-	eg. 	
+	eg.
 	for x = 1, 26 do
-		if passed_Arg == tbl_ALPHA[x] then				
-			return true;								
-		end			
+		if passed_Arg == tbl_ALPHA[x] then
+			return true;
+		end
 	end
 	return false;
 ]]--
 function IsUpper(char)
-	local asc = string.byte(char);	
-	if asc >= 65 and asc <= 90 then	return true 
+	local asc = string.byte(char);
+	if asc >= 65 and asc <= 90 then	return true
 	else return false end
 end
 
 function IsLower(char)
-	local asc = string.byte(char);	
-	if asc >= 97 and asc <= 122 then return true 
+	local asc = string.byte(char);
+	if asc >= 97 and asc <= 122 then return true
 	else return false end
 end
 
-function IsNumber(char)	
+function IsNumber(char)
 	if string.find(char, "%d") ~= nil then return true
 	else return false end
 end
 
 function posInPass(char)	--Calculate position in the passtable
-	if string.find(char, "%d") ~= nil then 
+	if string.find(char, "%d") ~= nil then
 		return tonumber(char)
 	else
 		local asc = string.byte(char)
 		if not asc then
 			return 0
-		elseif asc >= 65 and asc <= 90 then 
+		elseif asc >= 65 and asc <= 90 then
 			return (asc - 64)
-		elseif asc >= 97 and asc <= 122 then 
+		elseif asc >= 97 and asc <= 122 then
 			return (asc - 96)
 		end
 	end
-	
+
 	return 0
 end
 
 --#######################################################################
 
 function Shar_Encrypt(pText, pass, encNum)
-	
+
 	--Initialize Variables
 	if not pass then pass = "" end
 	if not encNum then encNum = false end
-	
+
 	encText = "";							--The encrypted text, currently "Nothing"
 	pTextLen = #pText;						--The length of plain text
 
@@ -77,13 +77,13 @@ function Shar_Encrypt(pText, pass, encNum)
 
 	for e = 1, pTextLen do
 
-		posInAlpha = 0;						--Position of the character in Lowercase alphabets	
+		posInAlpha = 0;						--Position of the character in Lowercase alphabets
 		posInAlphaCaps = 0;					--Position in Uppercase alphabets
 		posInString = 0;					--Position in Plain Text
 
 		nChar = string.sub(pText, e, e);	--Get the string character by character
-		
-		if passCntr <= passLen then passCntr = passCntr + 1 end	--DO NOT convert these conditions to 
+
+		if passCntr <= passLen then passCntr = passCntr + 1 end	--DO NOT convert these conditions to
 		if passCntr > passLen then passCntr = 1 end				--if..else.. or the results would vary
 
 		nPass = string.sub(pass, passCntr, passCntr);	--Get the password character by character
@@ -99,68 +99,68 @@ function Shar_Encrypt(pText, pass, encNum)
 				break;
 			end
 		end
-]]--		
+]]--
 		local asc = string.byte(nChar);		--The actual position in the alphabets
 		if asc >= 65 and asc <= 90 then posInAlphaCaps = asc - 64 else posInAlphaCaps = 0 end
-		if asc >= 97 and asc <= 122 then posInAlpha = asc - 96 else posInAlpha = 0 end	
-		
-		posInString = e;					--The actual position in the passed string		
+		if asc >= 97 and asc <= 122 then posInAlpha = asc - 96 else posInAlpha = 0 end
 
---[[	
-		This is the main feature of this cipher, An idea of my friend RAVI KUMAR. 
-		
-		What we do here is: Add all the positions(Alpha, Caps, String & Pass) so as a result 
+		posInString = e;							--The actual position in the passed string
+
+--[[
+		This is the main feature of this cipher, An idea of my friend RAVI KUMAR.
+
+		What we do here is: Add all the positions(Alpha, Caps, String & Pass) so as a result
 		we get different encrypted text which varies with the string and the password used.
-		
+
 		for e.g.
 		The cipher text of 'a' in strings 'Shadab' and 'Ravi' will vary as in the first
 		string 'a' occurs at 3rd & 5th position while in second string it occurs at 2nd position.
-		
+
 		Either one of posInAlpha or posInAlphaCaps will always be zero.
 ]]--
 		sumOfPos = posInAlpha + posInAlphaCaps + posInString + posInPass(nPass);
 
---Debug.Print("nChar = "..nChar.."\r\n");		
---Debug.Print("nPass = "..nPass.."\r\n");	
---Debug.Print("posInPass = "..posInPass(nPass).."\r\n");	
---Debug.Print("posInAlpha = "..posInAlpha.."\r\n");	
---Debug.Print("posInAlphaCaps = "..posInAlphaCaps.."\r\n");	
---Debug.Print("posInString = "..posInString.."\r\n");		
+--Debug.Print("nChar = "..nChar.."\r\n");
+--Debug.Print("nPass = "..nPass.."\r\n");
+--Debug.Print("posInPass = "..posInPass(nPass).."\r\n");
+--Debug.Print("posInAlpha = "..posInAlpha.."\r\n");
+--Debug.Print("posInAlphaCaps = "..posInAlphaCaps.."\r\n");
+--Debug.Print("posInString = "..posInString.."\r\n");
 --Debug.Print("sumOfPos = "..sumOfPos.."\r\n");
-		
+
 		sumOfPos = sumOfPos % 26		--Handle Characters greater than 26, (mod 26)
 		if sumOfPos == 0 then sumOfPos = 26 end
 
 --Debug.Print("NewSumOfPos = "..sumOfPos.."\r\n");
-			
+
 		if IsUpper(nChar) then								--Uppercase Chars
-			encText = encText..tbl_SHARCAPS[sumOfPos];					
+			encText = encText..tbl_SHARCAPS[sumOfPos];
 		elseif IsLower(nChar) then							--Lowercase chars
-			encText = encText..tbl_SHAR[sumOfPos];			
-		elseif IsNumber(nChar) and encNum then		--Numbers	
+			encText = encText..tbl_SHAR[sumOfPos];
+		elseif IsNumber(nChar) and encNum then			--Numbers
 			local num = tonumber(nChar);
 			eNum = num + posInString;
-			
-			while eNum > 9 do								--Make the number unary i.e 26 becomes 8 (2+6)			
+
+			while eNum > 9 do									--Make the number unary i.e 26 becomes 8 (2+6)
 				local eNumStr = tostring(eNum);
 				eNum = 0;
-				
+
 				for o = 1, string.len(eNumStr) do
 					num = tonumber(string.sub(eNumStr, o, o));
-					eNum = eNum + num;					
-				end				
-			end			
+					eNum = eNum + num;
+				end
+			end
 			encText = encText..eNum;
 		else
-			encText = encText..nChar;						--Un-Encrypted Text		
-		end	
-		
---Debug.Print("encText = "..encText.."\r\n\r\n");	
-		
+			encText = encText..nChar;						--Un-Encrypted Text
+		end
+
+--Debug.Print("encText = "..encText.."\r\n\r\n");
+
 	end --End of FOR LOOP
-	
+
 	return encText;
-	
+
 end --End Of FUNCTION
 
 -----------------------------------------------
@@ -170,46 +170,46 @@ function Shar_Decrypt(cText, pass, decNum)
 	--Initialize Variables
 	if not pass then pass = "" end
 	if not encNum then encNum = false end
-	
-	local cTextLen = string.len(cText);	
+
+	local cTextLen = string.len(cText);
 	local decText = "";
 
 	passLen = string.len(pass);				--Length of Password
-	passCntr = 0; 							--Pass Counter						
-		
+	passCntr = 0; 									--Pass Counter
+
 	for p = 1, cTextLen do
-	
+
 		posInShar = 0;
 		posInSharCaps = 0;
 		posInString = 0;
-				
+
 		nChar = string.sub(cText, p, p);
-		
-		if passCntr <= passLen then passCntr = passCntr + 1 end	--DO NOT convert these conditions to 
+
+		if passCntr <= passLen then passCntr = passCntr + 1 end	--DO NOT convert these conditions to
 		if passCntr > passLen then passCntr = 1 end				--	if..else.. or the results would vary.
 
 		nPass = string.sub(pass, passCntr, passCntr);
-				
+
 		for q = 1, 26 do
-			if nChar == tbl_SHAR[q] then	
+			if nChar == tbl_SHAR[q] then
 				posInShar = q;
 				break;
 			end
 		end
 
 		for r = 1, 26 do
-			if nChar == tbl_SHARCAPS[r] then			
+			if nChar == tbl_SHARCAPS[r] then
 				posInSharCaps = r;
 				break;
 			end
 		end
-		
+
 		posInString = p;
-				
+
 		diffOfPos = posInShar + posInSharCaps - posInString - posInPass(nPass);
 
 		while diffOfPos <= 0 do diffOfPos = diffOfPos + 26 end
-		
+
 --Debug.Print("nChar = "..nChar.."\r\n");
 --Debug.Print("nPass = "..nPass.."\r\n");
 --Debug.Print("posInPass = "..posInPass(nPass).."\r\n");
@@ -223,20 +223,20 @@ function Shar_Decrypt(cText, pass, decNum)
 			decText = decText..tbl_ALPHACAPS[diffOfPos];
 		elseif IsLower(nChar) then									--LowerCase Chars
 			decText = decText..tbl_ALPHA[diffOfPos];
-		elseif IsNumber(nChar) and decNum then		--Numbers	
+		elseif IsNumber(nChar) and decNum then					--Numbers
 			local num = tonumber(nChar);
-			dNum = num - posInString;			
-			while dNum <= 0 do dNum = dNum + 9 end						
-			decText = decText..dNum;	
+			dNum = num - posInString;
+			while dNum <= 0 do dNum = dNum + 9 end
+			decText = decText..dNum;
 		else
-			decText = decText..nChar;							--Un-Encrypted Text
+			decText = decText..nChar;								--Un-Encrypted Text
 		end
-				
+
 --Debug.Print("decText = "..decText.."\r\n\r\n");
-		
+
 	end
-	
-	return decText;	
+
+	return decText;
 end
 
 --################################################################################################################
@@ -249,4 +249,3 @@ end
 --file:close(); file2:close();
 
 print(Shar_Encrypt("Shadab Zafar", "1", false))
--- print(Shar_Decrypt(Shar_Encrypt("Bomb Phodna Hai Jama Masjid Pe", "ujmko", false), "ujmko", false))
